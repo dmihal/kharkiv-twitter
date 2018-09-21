d7c066d2a31b2321f424de801066ac950f53ed33
David Mihal <david@dmihal.com>
2018-09-21 17:14:22 +0100

    Allow users to post tweets

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
 import Typography from '@material-ui/core/Typography';
+import Compose from './Compose';
 import Header from './Header';
 import Feed from './Feed';
 
-const App = () => (
+const App = ({ user }) => (
   <div>
     <Header />
     <Typography variant="display3">Welcome to Meteor!</Typography>
+    {user && (
+      <Compose />
+    )}
     <Feed />
   </div>
 );
 
-export default App;
+const tracker = () => {
+  return {
+    user: Meteor.user(),
+  };
+};
+
+export default withTracker(tracker)(App);
